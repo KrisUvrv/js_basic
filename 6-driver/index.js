057ef50d81ebabdeeea3423741484e5bcc662b78
KrisUvrv <kduvarova@gmail.com>
2024-07-13 02:34:39 +0100

    fix

--- 6-driver/index.js
+++
@@ -1,6 +0,0 @@
-const hasLicence = true;
-const age = 18;
-const isDrunk = false;
-
-const canDrive = hasLicence && age >= 18 && !isDrunk;
-console.log(`Может ли сесть за руль: ${canDrive ? 'Да' : 'Нет'}`);
